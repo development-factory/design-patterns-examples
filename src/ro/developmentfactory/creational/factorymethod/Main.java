ca6d1032f30f11e70ee6bc93a982fc8113bc6cd0
DevFactory <devfact.tech@gmail.com>
2021-03-19 17:35:14 +0200

    changed os picking in factory method to switch

--- src/ro/developmentfactory/creational/factorymethod/Main.java
+++ src/ro/developmentfactory/creational/factorymethod/Main.java
@@ -5,12 +5,16 @@ public class Main {
     public static void main(String[] args) {
         // Setting configuration based on operating system.
         String osName = System.getProperty("os.name");
-        if(osName.equals("Mac OS X")){
-            notifier = new MacOsNotifier();
-        }else if(osName.equals("Windows 10")){
-            notifier = new WindowsNotifier();
-        }else if(osName.equals("Ubuntu")){
-            notifier = new LinuxNotifier();
+        switch (osName) {
+            case "Mac OS X":
+                notifier = new MacOsNotifier();
+                break;
+            case "Windows 10":
+                notifier = new WindowsNotifier();
+                break;
+            case "Ubuntu":
+                notifier = new LinuxNotifier();
+                break;
         }
 
         // Sending notification
